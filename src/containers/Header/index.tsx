3b22f73e1e53004f31caa2964a13c4658d584556
JJrCosta <jrpercosta@gmail.com>
2023-12-29 14:46:13 -0300

    Fix content component

--- src/containers/Header/index.tsx
+++ src/containers/Header/index.tsx
@@ -1,9 +1,20 @@
-import { Head, Title } from './styles'
+import SearchBar from '../../components/SearchBar'
+import { Container } from '../../styles'
+import { Head, AlignContainer, Logo, Title, BtnAddNew } from './styles'
 
 const Header = () => {
   return (
     <Head>
-      <Title>contatos</Title>
+      <Container>
+        <AlignContainer>
+          <AlignContainer>
+            <Logo />
+            <Title>contatos</Title>
+          </AlignContainer>
+          <SearchBar />
+          <BtnAddNew>+ Novo</BtnAddNew>
+        </AlignContainer>
+      </Container>
     </Head>
   )
 }
