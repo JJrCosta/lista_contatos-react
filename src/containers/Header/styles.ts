b2751ba350f900bdf7118f19c60fa4331b5b6f53
JJrCosta <jrpercosta@gmail.com>
2023-12-29 14:46:48 -0300

    Fix content component

--- src/containers/Header/styles.ts
+++ src/containers/Header/styles.ts
@@ -1,15 +1,39 @@
 import styled from 'styled-components'
 
 export const Head = styled.header`
-  text-align: center;
   background-color: #333;
   padding: 16px;
 `
 
+export const Logo = styled.img`
+  background-color: #fff;
+  height: 50px;
+  width: 50px;
+  border-radius: 50%;
+  margin-right: 16px;
+`
+
 export const Title = styled.h1`
-  font-size: 32px;
+  display: inline-block;
+  font-size: 24px;
   color: #fff;
   text-transform: uppercase;
   font-weight: normal;
   letter-spacing: 2px;
+`
+
+export const AlignContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+export const BtnAddNew = styled.button`
+  border: none;
+  background-color: #6b9e4b;
+  padding: 8px 16px;
+  font-size: 18px;
+  color: #fff;
+  cursor: pointer;
+  border-radius: 2px;
 `
